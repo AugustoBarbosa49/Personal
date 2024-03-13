897cd18211fc2e4deb34bd5d26d4fa2b5f38735a
Augusto Barbosa <augusto.barbosa18@gmail.com>
2024-03-13 13:04:40 -0300

    Versão comentada do código

--- Separacao de textos e conversao para tabelas excel.py
+++ Separacao de textos e conversao para tabelas excel.py
@@ -1,23 +1,22 @@
 import pandas as pd
 import os
 
+# Instruções inciais
 print("\n",
-    "Este código consegue transformar textos em tabelas", 
-      "\n", "desde que esses dados estejam separados de maneira organizada", 
-      "\n", "e para isso você pode escolher colar um texto, abrir um arquivo", 
+    "Este código consegue transformar textos em tabelas,", 
+      "\n", "desde que esses dados estejam separados de maneira organizada.", 
+      "\n", "Para isso você pode escolher colar um texto, abrir um arquivo", 
       "\n",  ".csv ou ainda um arquivo .txt.",
       "\n", "No caso de arquivos (.csv ou .txt), por garantia, procure utilizar",
-      "\n", "o local exato dos arquivos e não apenas o nome dele.",
-      "\n", "A fim de evitar possíveis erros, para as opções de colar um texto",
-      "\n", "e arquivos .txt, as colunas serão automaticamente entituladas com ",
-      "\n", "números inteiros começando em 1.",
-      "\n")
+      "\n", "o local exato dos arquivos e não apenas o nome dele.")
 
+# Altera o local onde o python vai trabalhar
 localSalvar = input("Antes de começar, insira o local de seu computador onde quer que a planilha seja salva: ")
 os.chdir(localSalvar)
 
-opcao = input("Digite 'texto' para colar um texto próprio, 'csv' para, usar um aquivo .csv ou 'txt' para usar um arquivo .txt: ")
+opcao = input("Digite 'colar' para colar um texto próprio, 'csv' para, usar um aquivo .csv ou 'txt' para usar um arquivo .txt: ")
 
+# Procedimentos para separação de textos e arquivos .txt
 def SeparaTextos(Texto):
     separadorLinhas = input("Insira o separador que divide as linhas do texto: ")
     separadorColunas = input("Insira o separador que divide as colunas do texto: ")
@@ -27,6 +26,7 @@ def SeparaTextos(Texto):
 
     if separadorColunas != separadorLinhas:
 
+        # Para cada linha,  os dados serão separados em uma lista e essa lista é adicionada a outra lista
         dados = []
         i = 0
         while i < len(Texto):
@@ -34,11 +34,13 @@ def SeparaTextos(Texto):
             dados.append(NovaLinha)
             i += 1
 
+        # Descobre o tamanho da maior linha
         tamanho = []
         for d in dados:
             tamanho.append(len(d))
         tamanho = max(tamanho)
 
+        # Nomeia as colunas necessárias, baseado no tamanho da maior linha
         i = 1
         Colunas = []
         while i <= tamanho:
@@ -54,28 +56,40 @@ def SeparaTextos(Texto):
         linha = []
         n = 1
         for i in Texto:
-            if n/nCol > 1:
+            if n/nCol > 1: # Se o elemento analisado estiver numa posição além do número de colunas determinadas, a linha será pulada para a próxima
                 dados.append(linha)
                 linha = []
                 n = 2
                 linha.append(i)
-            elif i == Texto[-1]:
+            elif i == Texto[-1]: # Se o elemento analisado for o último da lista ele e sua linha serão adicionados aos dados antes do fim da repetição
                 linha.append(i)
                 dados.append(linha)
             else:
                 linha.append(i)
                 n += 1
 
+        # Nomeia as colunas baseada no número fornecido
         i = 1
         Colunas = []
         while i <= nCol:
             Colunas.append(i)
             i += 1
-        
+
+    # Permite o usuário nomear as colunas criadas  
+    nomear = input("Escreva 'S' se deseja nomear as colunas da tabela: ")
+
+    if nomear == 'S':
+        nomes = []
+        for i in Colunas:
+            print('Digite o nome da coluna número ', i, ":")
+            a = input()
+            nomes.append(a)
+        Colunas = nomes
+
     return pd.DataFrame(data=dados, columns = Colunas)
 
-match opcao:
-    case "texto":
+match opcao:    # Análise das opção escolhida pelo usuário
+    case "colar":
         Texto = input("Insira o texto a ser separado: ")
 
         Tabela = SeparaTextos(Texto)
@@ -87,7 +101,9 @@ match opcao:
     case "csv":
         arquivo = input("Insira o local/nome do arquivo csv que deseja converter: ")
         Tabela = pd.read_csv(arquivo, encoding='unicode_escape')
+
         Tabela.to_excel("csv Transformado.xlsx")
+        
         print("A separação ocorreu com sucesso!")
 
     case "txt":
